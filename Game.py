6701e66702205f58f176611a7af2b9e68fa8e812
dano34558 <dano34558@gmail.com>
2023-02-15 07:40:08 -0600

    initial commits

--- Game.py
+++ Game.py
@@ -3,72 +3,54 @@ from Player import Player
 from Army import Army
 from Enemy import Enemy
 from EventManager import EventManager
+from Ui import UI
+import pygame
+import random
+from Player import Player
+from Enemy import Enemy
+from EventManager import EventManager
+
 
 class Game:
     def __init__(self):
-        self.width = 640
-        self.height = 480
-        self.fps = 60
-        self.screen = pygame.display.set_mode((self.width, self.height))
-        self.clock = pygame.time.Clock()
-        self.player = Player("Player 1",(0,0))
-        self.enemy = Enemy("Enemy", Army(""))
-        self.event_manager = EventManager(self.player, self.enemy)
+        # initialize Pygame
+        pygame.init()
+
+        # set up window
+        self.screen_width = 800
+        self.screen_height = 600
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption("Pygame Adventure")
+
+        # initialize game objects
         self.font = pygame.font.SysFont('Arial', 16)
-        self.run_event_button = pygame.Rect(20, 400, 200, 50)
-        self.done = False
-    
+        self.clock = pygame.time.Clock()
+        self.player = Player('Player 1', 100, 10, 5)
+        self.enemy = Enemy("stronk",Army(["hubert"]))
+        self.event_manager = EventManager(self.player,self.enemy)
+        self.ui = UI(self.screen, self.font, self.player, self.enemy, self.event_manager)
+
     def run(self):
-        while not self.done:
-            self._handle_events()
-            self._update()
-            self._draw()
+        # main game loop
+        while True:
+            # handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.ui.run_event_button.rect.collidepoint(event.pos):
+                        self.event_manager.run_random_event()
+
+            # update game objects
+            self.ui.update()
+
+            # draw screen
+            self.screen.fill((255, 255, 255))
+            self.ui.draw()
+
+            # update display
             pygame.display.flip()
-            self.clock.tick(self.fps)
 
-    def _handle_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.done = True
-            elif event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                if self.run_event_button.collidepoint(pos):
-                    self.event_manager.run_event()
-
-    def _update(self):
-        pass
-
-    def _draw(self):
-        self.screen.fill((255, 255, 255))
-
-        # Draw player stats
-        text = self.font.render(f"Player: {self.player.name}", True, (0, 0, 0))
-        self.screen.blit(text, (20, 20))
-        text = self.font.render(f"Gold: {self.player.gold}", True, (0, 0, 0))
-        self.screen.blit(text, (20, 40))
-        text = self.font.render(f"Experience: {self.player.experience}", True, (0, 0, 0))
-        self.screen.blit(text, (20, 60))
-
-        # Draw army stats
-        text = self.font.render("Army", True, (0, 0, 0))
-        self.screen.blit(text, (20, 100))
-        y = 120
-        for soldier in self.player.army:
-            text = self.font.render(f"{soldier.name} ({soldier.type}), Level {soldier.level}, Health {soldier.health}/{soldier.max_health}", True, (0, 0, 0))
-            self.screen.blit(text, (20, y))
-            y += 20
-
-        # Draw run event button
-        pygame.draw.rect(self.screen, (0, 255, 0), self.run_event_button)
-        text = self.font.render("Run Event", True, (0, 0, 0))
-        self.screen.blit(text, (30, 415))
-
-    def _quit(self):
-        pygame.quit()
-
-if __name__ == '__main__':
-    pygame.init()
-    pygame.display.set_caption("Adventure Game")
-    game = Game()
-    game.run()
-    game._quit()
+            # tick clock
+            self.clock.tick(60)
